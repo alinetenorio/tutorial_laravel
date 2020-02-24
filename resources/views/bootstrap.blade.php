cac904de7ed710ad77c04fed8a2cb5e976e15015
alinetenorio <aline.tenorio96@gmail.com>
2020-02-24 18:12:41 -0300

    adding bootstrap

---
+++ resources/views/bootstrap.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html lang=”pt-br”>
+<head>
+<meta charset=”UTF-8”>
+<title>asdf</title>
+
+<link rel='stylesheet' href="{{ asset('site/style.css') }}">
+</head>
+<body>
+
+<h1>teste</h1>
+
+<button class="btn btn-lg btn-king-color">boo</button>
+
+<script src="{{ asset('site/jquery.js') }}"></script>
+<script src="{{ asset('site/bootstrap.js') }}"></script>
+</body>
+</html>
